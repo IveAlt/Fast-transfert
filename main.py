e2c2ab5e03730d730ca91df3e7bc8242a4e26fda
IveAlt <106318189+IveAlt@users.noreply.github.com>
2024-06-17 15:21:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,27 +14,27 @@ user = tk.StringVar()
 user2 = tk.StringVar()
 same_user = tk.IntVar()
 
-tk.Label(root, text='Entrer IP Poste 1 :').grid(row=0, column=0)
+tk.Label(root, text='Enter IP PC 1 :').grid(row=0, column=0)
 tk.Entry(root, textvariable=IP1).grid(row=0, column=1)
 
-tk.Label(root, text='Utilisateur  :').grid(row=1, column=0)
+tk.Label(root, text='User  :').grid(row=1, column=0)
 tk.Entry(root, textvariable=user).grid(row=1, column=1)
 
-user2_label = tk.Label(root, text='Utilisateur 2 :')
+user2_label = tk.Label(root, text='User 2 :')
 user2_label.grid(row=2, column=0)
 user2_entry = tk.Entry(root, textvariable=user2)
 user2_entry.grid(row=2, column=1)
 
-tk.Label(root, text='Entrer IP Poste 2 :').grid(row=0, column=2)
+tk.Label(root, text='Enter IP PC 2 :').grid(row=0, column=2)
 tk.Entry(root, textvariable=IP2).grid(row=0, column=3)
 
-Bureau = tk.IntVar()
+Desktop = tk.IntVar()
 Documents = tk.IntVar()
-Téléchargement = tk.IntVar()
+Downloads = tk.IntVar()
 
-tk.Checkbutton(root, text='Bureau', variable=Bureau).place(relx=.3, rely=.1)
+tk.Checkbutton(root, text='Desktop', variable=Desktop).place(relx=.3, rely=.1)
 tk.Checkbutton(root, text='Documents', variable=Documents).place(relx=.3, rely=.2)
-tk.Checkbutton(root, text='Téléchargement', variable=Téléchargement).place(relx=.3, rely=.3)
+tk.Checkbutton(root, text='Downloads', variable=Downloads).place(relx=.3, rely=.3)
 
 def toggle_user2():
     if same_user.get():
@@ -44,7 +44,7 @@ def toggle_user2():
         user2_entry.grid()
         user2_label.grid()
 
-tk.Checkbutton(root, text='Même Utilisateur', variable=same_user, command=toggle_user2).place(relx=.0, rely=.4)
+tk.Checkbutton(root, text='Same User', variable=same_user, command=toggle_user2).place(relx=.0, rely=.4)
 
 subprocesses = []
 
@@ -55,7 +55,7 @@ def long_running_task():
         if same_user.get():
             user2.set(user.get())
         filetrsf_process = subprocess.Popen(
-            ['python', 'filetrsf.py', IP1.get(), user.get(), user2.get(), IP2.get(), str(Bureau.get()), str(Documents.get()), str(Téléchargement.get())],
+            ['python', 'filetrsf.py', IP1.get(), user.get(), user2.get(), IP2.get(), str(Desktop.get()), str(Documents.get()), str(Downloads.get())],
             stdout=f,
             stderr=subprocess.STDOUT,
             bufsize=1,
